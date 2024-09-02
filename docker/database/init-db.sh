12195b6ca4c2f0ce9fe5fd1c2ef78c9c171f9534
shileshsivaseyon <Shilesh.Sivaseyon@HMCTS.NET>
2024-09-02 16:08:23 +0100

    Create a Judgment Reporting DB

--- docker/database/init-db.sh
+++ docker/database/init-db.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-# Civil SDT Repl database
-if [ -z "$SDT_DB_PASSWORD" ] || [ -z "$SDT_DB_USERNAME" ]; then
-  echo "ERROR: Missing environment variable. Set value for both 'SDT_DB_USERNAME' and 'SDT_DB_PASSWORD'."
+# Civil Judgement Feed database
+if [ -z "$JF_DB_PASSWORD" ] || [ -z "$JF_DB_USERNAME" ]; then
+  echo "ERROR: Missing environment variable. Set value for both 'SDT_DB_USERNAME' and 'JF_DB_PASSWORD'."
   exit 1
 fi
 
-psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=${SDT_DB_USERNAME} --set PASSWORD=${SDT_DB_PASSWORD} <<-EOSQL
-  CREATE USER :USERNAME WITH PASSWORD ':PASSWORD';
+psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=${JF_DB_USERNAME} --set PASSWORD=${JF_DB_PASSWORD} <<-EOSQL
+  CREATE USER :USERNAME WITH PASSWORD :'PASSWORD';
 
-  CREATE DATABASE civil_sdt
+  CREATE DATABASE civil_jf
     WITH OWNER = :USERNAME
     ENCODING = 'UTF-8'
     CONNECTION LIMIT = -1;
---
+++ src/main/resources/db/migration/V0001__init.sql
@@ -0,0 +1,65 @@
+SET search_path TO public;
+
+------------------------------------------------
+-- Create tables
+------------------------------------------------
+CREATE TABLE judgements
+(id BIGINT,
+service_id VARCHAR(4),
+judgement_id VARCHAR(42),
+judgement_event_timestamp TIMESTAMP(2),
+court_code VARCHAR(3),
+ccd_case_ref VARCHAR(16),
+case_number VARCHAR(8),
+judgement_admin_order_total NUMERIC(8,2),
+judgement_admin_order_date DATE,
+registration_type VARCHAR(1),
+cancellation_date DATE,
+defendant_name VARCHAR(70),
+defendant_address_line_1 VARCHAR(35),
+defendant_address_line_2 VARCHAR(35),
+defendant_address_line_3 VARCHAR(35),
+defendant_address_line_4 VARCHAR(35),
+defendant_address_line_5 VARCHAR(35),
+defendant_address_postcode VARCHAR(8),
+defendant_dob DATE,
+reported_to_rtl TIMESTAMP(2)
+);
+
+------------------------------------------------
+-- Create Primary Keys
+------------------------------------------------
+ALTER TABLE judgements ADD CONSTRAINT j_i_pk PRIMARY KEY USING INDEX judgements_pk;
+
+------------------------------------------------
+-- Create Check Constraints
+------------------------------------------------
+ALTER TABLE judgements ADD CONSTRAINT j_si_nn CHECK (service_id IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT judgement_id CHECK (judgement_id IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT judgement_event_timestamp CHECK (judgement_event_timestamp IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT court_code CHECK (court_code IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT ccd_case_ref CHECK (ccd_case_ref IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT case_number CHECK (case_number IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT judgement_admin_order_total (judgement_admin_order_total IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT judgement_admin_order_date (judgement_admin_order_date IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT registration_type (registration_type IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT defendant_name (defendant_name IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT defendant_address_line_1 (defendant_address_line_1 IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT defendant_address_postcode (defendant_address_postcode IS NOT NULL);
+
+
+------------------------------------------------
+-- Create indices
+------------------------------------------------
+CREATE UNIQUE INDEX judgements_pk ON judgements (id);
+
+
+------------------------------------------------
+-- Create Unique constraints for PUBLIC
+------------------------------------------------
+ALTER TABLE judgements ADD CONSTRAINT j_si_ji_uniq UNIQUE (service_id, judgement_id);
+
+
+-- Create Sequences for PUBLIC
+------------------------------------------------
+CREATE SEQUENCE judg_seq MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH 1 NO CYCLE;
