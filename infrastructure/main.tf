05a2680cacca69eb150bfe4769917991bda4430d
shileshsivaseyon <Shilesh.Sivaseyon@HMCTS.NET>
2024-09-13 15:06:07 +0100

    CRF-9-key-vault infrastructure change
    Creating key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,18 @@
+locals {
+  tags = (merge(
+    var.common_tags,
+    tomap({
+      "Team Contact" = var.team_contact
+      "Destroy Me"   = var.destroy_me
+    })
+  ))
+}
+
 provider "azurerm" {
   features {}
+}
+
+resource "azurerm_resource_group" "civil_rtl_export_rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = var.location
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,3 +14,24 @@ variable "common_tags" {
   type = map(string)
 }
 
+variable "team_contact" {
+  type        = string
+  description = "The name of your Slack channel people can use to contact your team about your infrastructure"
+  default     = "#civil-sdt"
+}
+
+variable "destroy_me" {
+  type        = string
+  description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
+  default     = "No"
+}
+
+variable "aks_subscription_id" {
+  type        = string
+  description = "The AKS subscription id for the environment.  Set by pipeline."
+}
+
+variable "jenkins_AAD_objectId" {
+  type        = string
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
