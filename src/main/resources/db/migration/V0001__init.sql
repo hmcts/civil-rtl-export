8a06a76cfec03d5805a4c19df388488c613d6e2d
shileshsivaseyon <Shilesh.Sivaseyon@HMCTS.NET>
2024-09-23 18:16:03 +0100

    Reformatting of index,constraint and sequence names

--- src/main/resources/db/migration/V0001__init.sql
+++ src/main/resources/db/migration/V0001__init.sql
@@ -30,39 +30,39 @@ reported_to_rtl TIMESTAMP(2)
 ------------------------------------------------
 -- Create indices
 ------------------------------------------------
-CREATE UNIQUE INDEX judgements_pk ON judgements (id);
+CREATE UNIQUE INDEX jud_pk ON judgements (id);
 
 
 ------------------------------------------------
 -- Create Primary Keys
 ------------------------------------------------
-ALTER TABLE judgements ADD CONSTRAINT j_i_pk PRIMARY KEY USING INDEX judgements_pk;
+ALTER TABLE judgements ADD CONSTRAINT jud_ind_pk PRIMARY KEY USING INDEX jud_pk;
 
 
 ------------------------------------------------
 -- Create Check Constraints
 ------------------------------------------------
-ALTER TABLE judgements ADD CONSTRAINT j_si_nn CHECK (service_id IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT judgement_id CHECK (judgement_id IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT judgement_event_timestamp CHECK (judgement_event_timestamp IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT court_code CHECK (court_code IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT ccd_case_ref CHECK (ccd_case_ref IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT case_number CHECK (case_number IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT judgement_admin_order_total CHECK (judgement_admin_order_total IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT judgement_admin_order_date CHECK (judgement_admin_order_date IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT registration_type CHECK (registration_type IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT defendant_name CHECK (defendant_name IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT defendant_address_line_1 CHECK (defendant_address_line_1 IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT defendant_address_postcode CHECK (defendant_address_postcode IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_ser_id_nn CHECK (service_id IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_jud_ind_nn CHECK (judgement_id IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_jud_eve_tim_nn CHECK (judgement_event_timestamp IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_cou_code_nn CHECK (court_code IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_ccd_cas_ref_nn CHECK (ccd_case_ref IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_cas_num_nn CHECK (case_number IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_adm_ord_tot_nn CHECK (judgement_admin_order_total IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_adm_ord_dat_nn CHECK (judgement_admin_order_date IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_reg_typ_nn CHECK (registration_type IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_def_nam_nn CHECK (defendant_name IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_def_add_lin_1_nn CHECK (defendant_address_line_1 IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_def_add_pos_nn CHECK (defendant_address_postcode IS NOT NULL);
 
 
 ------------------------------------------------
 -- Create Unique constraints for PUBLIC
 ------------------------------------------------
-ALTER TABLE judgements ADD CONSTRAINT j_si_ji_uniq UNIQUE (service_id, judgement_id);
+ALTER TABLE judgements ADD CONSTRAINT j_ser_id_jud_id_uni UNIQUE (service_id, judgement_id);
 
 
 ------------------------------------------------
 -- Create Sequences for PUBLIC
 ------------------------------------------------
-CREATE SEQUENCE judg_seq MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH 1 NO CYCLE;
+CREATE SEQUENCE jud_seq MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH 1 NO CYCLE;
