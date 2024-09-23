eda30fc3c4334c99cc311520e87ae147c5a9f1ad
shileshsivaseyon <Shilesh.Sivaseyon@HMCTS.NET>
2024-09-23 18:23:16 +0100

    Reformatting of index,constraint and sequence names

--- src/main/resources/db/migration/V0001__init.sql
+++ src/main/resources/db/migration/V0001__init.sql
@@ -30,26 +30,26 @@ reported_to_rtl TIMESTAMP(2)
 ------------------------------------------------
 -- Create indices
 ------------------------------------------------
-CREATE UNIQUE INDEX jud_pk ON judgements (id);
+CREATE UNIQUE INDEX jud_ind_pk ON judgements (id);
 
 
 ------------------------------------------------
 -- Create Primary Keys
 ------------------------------------------------
-ALTER TABLE judgements ADD CONSTRAINT jud_ind_pk PRIMARY KEY USING INDEX jud_pk;
+ALTER TABLE judgements ADD CONSTRAINT jud_pk PRIMARY KEY USING INDEX jud_ind_pk;
 
 
 ------------------------------------------------
 -- Create Check Constraints
 ------------------------------------------------
 ALTER TABLE judgements ADD CONSTRAINT j_ser_id_nn CHECK (service_id IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT j_jud_ind_nn CHECK (judgement_id IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_jud_id_nn CHECK (judgement_id IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_jud_eve_tim_nn CHECK (judgement_event_timestamp IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_cou_code_nn CHECK (court_code IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_ccd_cas_ref_nn CHECK (ccd_case_ref IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_cas_num_nn CHECK (case_number IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT j_adm_ord_tot_nn CHECK (judgement_admin_order_total IS NOT NULL);
-ALTER TABLE judgements ADD CONSTRAINT j_adm_ord_dat_nn CHECK (judgement_admin_order_date IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_jud_adm_ord_tot_nn CHECK (judgement_admin_order_total IS NOT NULL);
+ALTER TABLE judgements ADD CONSTRAINT j_jud_adm_ord_dat_nn CHECK (judgement_admin_order_date IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_reg_typ_nn CHECK (registration_type IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_def_nam_nn CHECK (defendant_name IS NOT NULL);
 ALTER TABLE judgements ADD CONSTRAINT j_def_add_lin_1_nn CHECK (defendant_address_line_1 IS NOT NULL);
