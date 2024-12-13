25be6bd532d84fda326274c6224f7e6ad8f207b0
ridoutp <paul.ridout@cgi.com>
2024-12-13 14:11:17 +0000

    CRF-12: Integration with location reference data
    Moved EnableFeignClients annotation to Application class to remove a circular dependency flagged by Sonar.

--- src/main/java/uk/gov/hmcts/reform/civil/config/RefDataLocationApiConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/RefDataLocationApiConfiguration.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.civil.config;
 
 import feign.codec.ErrorDecoder;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.gov.hmcts.reform.civil.client.RefDataLocationApi;
 import uk.gov.hmcts.reform.civil.client.RefDataLocationApiErrorDecoder;
 
 @Configuration
-@EnableFeignClients(clients = {RefDataLocationApi.class})
 public class RefDataLocationApiConfiguration {
 
     @Bean
