f40f45a2cf11fb0d4bc6c342c7056a4e09fc7f83
ridoutp <paul.ridout@cgi.com>
2024-12-16 08:43:04 +0000

    CRF-12: Integration with location reference data
    Renamed allowedFilterChain to serviceAuthExcludeFilterChain and changed it to use securityMatcher.
    
    Added additional security filter for all other requests.  This adds in the S2S ServiceAuthFilter.

--- src/main/java/uk/gov/hmcts/reform/civil/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/SecurityConfiguration.java
@@ -3,16 +3,21 @@ package uk.gov.hmcts.reform.civil.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    private static final String[] ALLOWED_LIST = {
+    private static final String[] SERVICE_AUTH_EXCLUDE_LIST = {
         "/health",
         "/health/liveness",
         "/"
@@ -26,9 +31,32 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain allowedFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth.requestMatchers(ALLOWED_LIST).permitAll())
-            .build();
+    @Order(1)
+    public SecurityFilterChain serviceAuthExcludeFilterChain(HttpSecurity http) throws Exception {
+        http
+            .securityMatcher(SERVICE_AUTH_EXCLUDE_LIST)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    @SuppressWarnings("java:S4502")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .addFilterBefore(serviceAuthFilter, AbstractPreAuthenticatedProcessingFilter.class)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 
     // TODO: Finish this class
