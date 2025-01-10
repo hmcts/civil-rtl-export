4fdf4eff2919f8b3197d26ae0679338909bdb29b
ridoutp <paul.ridout@cgi.com>
2025-01-10 16:22:33 +0000

    CRF-12: Fix Spring security configuration error
    Fixed error in Spring security configuration.
    
    Added basic tests for SecurityConfiguration.

---
+++ src/test/java/uk/gov/hmcts/reform/civil/config/SecurityConfigurationTest.java
@@ -0,0 +1,80 @@
+package uk.gov.hmcts.reform.civil.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SecurityConfigurationTest {
+
+    @Mock
+    private ServiceAuthFilter mockServiceAuthFilter;
+
+    @Mock
+    private HttpSecurity mockHttpSecurity;
+
+    @Mock
+    private DefaultSecurityFilterChain mockSecurityFilterChain;
+
+    private SecurityConfiguration securityConfiguration;
+
+    @BeforeEach
+    void setUp() {
+        securityConfiguration = new SecurityConfiguration(mockServiceAuthFilter);
+    }
+
+    @Test
+    void testServiceAuthExcludeFilterChain() throws Exception {
+        String[] authExcludeList = {"/health", "/health/liveness", "/health/readiness", "/"};
+
+        when(mockHttpSecurity.securityMatcher(authExcludeList)).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.authorizeHttpRequests(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.build()).thenReturn(mockSecurityFilterChain);
+
+        SecurityFilterChain filterChain = securityConfiguration.serviceAuthExcludeFilterChain(mockHttpSecurity);
+
+        assertSame(mockSecurityFilterChain, filterChain, "Unexpected SecurityFilterChain returned");
+
+        verify(mockHttpSecurity).securityMatcher(authExcludeList);
+        verify(mockHttpSecurity).authorizeHttpRequests(any());
+        verify(mockHttpSecurity).build();
+    }
+
+    @Test
+    void testFilterChain() throws Exception {
+        when(mockHttpSecurity.addFilterBefore(mockServiceAuthFilter, AbstractPreAuthenticatedProcessingFilter.class))
+            .thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.authorizeHttpRequests(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.sessionManagement(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.httpBasic(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.formLogin(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.logout(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.csrf(any())).thenReturn(mockHttpSecurity);
+        when(mockHttpSecurity.build()).thenReturn(mockSecurityFilterChain);
+
+        SecurityFilterChain filterChain = securityConfiguration.filterChain(mockHttpSecurity);
+
+        assertSame(mockSecurityFilterChain, filterChain, "Unexpected SecurityFilterChain returned");
+
+        verify(mockHttpSecurity).addFilterBefore(mockServiceAuthFilter, AbstractPreAuthenticatedProcessingFilter.class);
+        verify(mockHttpSecurity).authorizeHttpRequests(any());
+        verify(mockHttpSecurity).sessionManagement(any());
+        verify(mockHttpSecurity).httpBasic(any());
+        verify(mockHttpSecurity).formLogin(any());
+        verify(mockHttpSecurity).logout(any());
+        verify(mockHttpSecurity).csrf(any());
+        verify(mockHttpSecurity).build();
+    }
+}
