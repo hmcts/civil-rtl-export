3ad5eeb0f0618b808b72cbc072e458c8c8c7e5bd
ridoutp <paul.ridout@cgi.com>
2025-02-17 11:33:46 +0000

    CRF-4: Split findForUpdate JudgmentRepository methods
    Split findForUpdate() and findForUpdateServiceId() JudgmentRepository methods into two separate queries each.  This is to see if original queries are cause of issue seen when application is deployed to preview environment.

--- src/main/java/uk/gov/hmcts/reform/civil/repository/JudgmentRepository.java
+++ src/main/java/uk/gov/hmcts/reform/civil/repository/JudgmentRepository.java
@@ -13,20 +13,30 @@ import java.util.List;
 @Repository
 public interface JudgmentRepository extends JpaRepository<Judgment, Long> {
 
-    String PARAM_RERUN = "rerun";
     String PARAM_AS_OF = "asOf";
     String PARAM_SERVICE_ID = "serviceId";
 
     String PARAM_DATE_OF_DELETION = "dateOfDeletion";
 
-    @Query("SELECT j FROM Judgment j WHERE ((:rerun = true AND j.reportedToRtl = :asOf) "
-        + "OR (:rerun = false AND j.reportedToRtl IS NULL))")
-    List<Judgment> findForUpdate(@Param(PARAM_RERUN) boolean rerun, @Param(PARAM_AS_OF) LocalDateTime asOf);
+    default List<Judgment> findForUpdate(boolean rerun, LocalDateTime asOf) {
+        return rerun ? findForRtlRerun(asOf) : findForRtl();
+    }
 
-    @Query("SELECT j FROM Judgment j WHERE ((:rerun = true AND j.reportedToRtl = :asOf) "
-        + "OR (:rerun = false AND j.reportedToRtl IS NULL)) AND j.serviceId = :serviceId")
-    List<Judgment> findForUpdateByServiceId(@Param(PARAM_RERUN) boolean rerun,
-                                            @Param(PARAM_AS_OF) LocalDateTime asOf,
+    @Query("SELECT j FROM Judgment j WHERE j.reportedToRtl IS NULL")
+    List<Judgment> findForRtl();
+
+    @Query("SELECT j FROM Judgment j WHERE j.reportedToRtl = :asOf")
+    List<Judgment> findForRtlRerun(@Param(PARAM_AS_OF) LocalDateTime asOf);
+
+    default List<Judgment> findForUpdateByServiceId(boolean rerun, LocalDateTime asOf, String serviceId) {
+        return rerun ? findForRtlServiceIdRerun(asOf, serviceId) : findForRtlServiceId(serviceId);
+    }
+
+    @Query("SELECT j FROM Judgment j WHERE j.reportedToRtl IS NULL AND j.serviceId = :serviceId")
+    List<Judgment> findForRtlServiceId(@Param(PARAM_SERVICE_ID) String serviceId);
+
+    @Query("SELECT j FROM Judgment j WHERE j.reportedToRtl = :asOf AND j.serviceId = :serviceId")
+    List<Judgment> findForRtlServiceIdRerun(@Param(PARAM_AS_OF) LocalDateTime asOf,
                                             @Param(PARAM_SERVICE_ID) String serviceId);
 
     @Modifying
